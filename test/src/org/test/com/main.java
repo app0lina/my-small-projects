009350edaa364c3c556605bfac7009ba3072ebc3
app0lina <kizilova.polina@gmail.com>
2021-09-21 20:45:18 +0200

    Finished on 3:10:00

--- test/src/org/test/com/main.java
+++ test/src/org/test/com/main.java
@@ -2,7 +2,8 @@ package org.test.com;
 
 public class main {
     public static void main(String[] args) {
-        Phone iphone = new Phone("iphone", 45, 8, "mts", 648758);
-        
+        Car merdedes = new Car("mercedes11", 5, "red", new Engine("raneult", 8000));
+        Engine engin = merdedes.getEng();
+        System.out.println(engin);
     }
 }
