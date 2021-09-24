5cd28a93b22826f7a6a38dd1fa421aa5c576bd7a
app0lina <kizilova.polina@gmail.com>
2021-09-24 08:02:20 +0200

    started the inner classes

---
+++ test/src/org/test/com/test.java
@@ -0,0 +1,15 @@
+package org.test.com;
+
+public class test {
+    private int age;
+    private int name;
+
+    public class inner {
+        private String name;
+
+        public innermethod(String name) {
+            this.name = name;
+        }
+    }
+
+}
