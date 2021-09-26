f5ee2645f038ecf7a64bfc108ed37bffc9bed0fc
app0lina <kizilova.polina@gmail.com>
2021-09-26 20:41:27 +0200

    4:56:00 started the challenge (text in mi notes)

--- test/src/org/test/com/mainclass.java
+++ test/src/org/test/com/mainclass.java
@@ -1,10 +1,43 @@
 package org.test.com;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class mainclass {
     public static void main(String[] args) {
-        Stat test =  new Stat(23, "italian");
 
+        boolean finish = false;
+        while(!finish) {
+            System.out.println("Choose an option: \n\t1. Manage contacts \n\t2. Messages \n\t3. Quit");
+            Scanner sc = new Scanner(System.in);
+            int usopt = sc.nextInt();
+            switch (usopt) {
+                case 1:
+                    System.out.println("Choose an option: \n\t1. Manage contacts \n\t2. Messages \n\t3. Quit");
+                    usopt = sc.nextInt();
+                case 3: finish = true; break;
+                default: continue;
+            }
+        }
+
+/*
+* • Simulate your phone's contacts and messages applications
+
+Greet the user •
+Show these 3 options: 1. Manage contacts 2. Messages 3. Quit •
+
+ In case of selecting 1--> show these options:
+ *  1. Show all contacts
+ *  2. Add a new contact
+ *  3. Search for a contact
+ *  4. Delete a contact
+ *  5. Go back to the previous menu •
+
+In case of 2 --> show these options:
+*   1. See the list of al1 messages
+*   2. Send a new message
+*   3. Go back to the previous menu
+
+In case of 3 --> Quit the app
+* */
     }
 }
